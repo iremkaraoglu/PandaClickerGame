76cba98772aa508d6e2c5e1d8bfae0dde61fffb3
iremkaraoglu <iremkaraoglu@otsimo.com>
2021-09-25 11:33:26 +0300

    Initial Commit

---
+++ PandaClicker iOS/GameViewController.swift
@@ -0,0 +1,43 @@
+//
+//  GameViewController.swift
+//  PandaClicker iOS
+//
+//  Created by Irem Karaoglu on 25.09.2021.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let scene = GameScene.newGameScene()
+
+        // Present the scene
+        let skView = self.view as! SKView
+        skView.presentScene(scene)
+        
+        skView.ignoresSiblingOrder = true
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+    }
+
+    override var shouldAutorotate: Bool {
+        return true
+    }
+
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            return .allButUpsideDown
+        } else {
+            return .all
+        }
+    }
+
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+}
